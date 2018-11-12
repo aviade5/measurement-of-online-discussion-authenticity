7fa8673a061bcd7cc7695200a6a4f734d5e1cea7
maorreu <maorreu@post.bgu.ac.il>
2018-11-12 13:43:04 +0200

    somweb-674 Reduction in OldCrawler Tweets Fixed output csv from old tweet crawler

--- bad_actors/commons/method_executor.py
+++ bad_actors/commons/method_executor.py
@@ -27,24 +27,24 @@ class Method_Executor(AbstractController):
             os.makedirs(output_folder_path)
         claim_to_tweet_count_rows = []
         claim_to_tweet_id_rows = []
-        post_ids = set(before_dict.keys())
-        for post_id in post_ids:
-            claim = claims_dict[post_id]
+        claim_ids = set(before_dict.keys() + after_dict.keys())
+        for claim_id in claim_ids:
+            claim = claims_dict[claim_id]
 
-            claim_content = claim.content
-            original_claim_keywords = claim.tags
+            claim_content = claim.description
+            original_claim_keywords = claim.keywords
             keywords = original_claim_keywords.split(",")
             claim_keywords = "||".join(keywords)
-            snopes_publication_date = date_to_str(claim.date)
-            tweet_ids_before = before_dict[post_id]
-            tweet_ids_after = after_dict[post_id]
+            snopes_publication_date = date_to_str(claim.verdict_date)
+            tweet_ids_before = before_dict[claim_id]
+            tweet_ids_after = after_dict[claim_id]
             tweet_before_count = len(tweet_ids_before)
             tweet_after_count = len(tweet_ids_after)
             tweet_combined_count = len(tweet_ids_before & tweet_ids_after)
             total_tweet_count = tweet_before_count + tweet_after_count - tweet_combined_count
             claim_to_tweet_count_rows.append(
-                (post_id, claim_content, claim_keywords, snopes_publication_date, tweet_before_count, tweet_after_count, tweet_combined_count, total_tweet_count))
-            claim_to_tweet_id_rows.append((post_id, claim_content, claim_keywords, snopes_publication_date, ','.join(tweet_ids_before), ','.join(tweet_ids_after)))
+                (claim_id, claim_content, claim_keywords, snopes_publication_date, tweet_before_count, tweet_after_count, tweet_combined_count, total_tweet_count))
+            claim_to_tweet_id_rows.append((claim_id, claim_content, claim_keywords, snopes_publication_date, ','.join(tweet_ids_before), ','.join(tweet_ids_after)))
 
         post_id_num_of_tweets_df = pd.DataFrame(claim_to_tweet_count_rows,
                                                 columns=['claim_id', 'claim_content', 'claim_keywords', 'snopes_publication_date', 'num_before', 'num_after', 'num_combine', 'total'])
