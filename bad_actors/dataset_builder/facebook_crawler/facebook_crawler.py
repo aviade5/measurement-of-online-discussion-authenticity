43d66b40af5ebb5fa775c6e4a91e9bd1fd1d86f8
NitzanMaarek <maarekn@post.bgu.ac.il>
2019-05-31 12:44:56 +0300

    Finished 'about' collection for specific user and for many users from given group id.
    Improved group member extraction to avoid pages that are members of the group.

--- bad_actors/dataset_builder/facebook_crawler/facebook_crawler.py
+++ bad_actors/dataset_builder/facebook_crawler/facebook_crawler.py
@@ -2,9 +2,11 @@
 
 from __future__ import print_function
 
+from trace import pickle
+
 from commons import commons
 from commons.method_executor import Method_Executor
-
+import os
 ## Nitzan Imports
 from telnetlib import EC
 from datetime import datetime
@@ -21,13 +23,10 @@ from DB.schema_definition import Author, AuthorConnection
 ### ******    ******    ******    ******    ******    ******    ******    ******    ******    ******    ******    ******
 ### ******    ******    ******    ******    ******    ******    ******    ******    ******    ******    ******    ******
 
-# Public methods begin with name "extract" and they include logging into facebook - beginning a logged in session
-
-# Make sure you know that (above) when using this class.
+# notes
 
 ### ******    ******    ******    ******    ******    ******    ******    ******    ******    ******    ******    ******
 ### ******    ******    ******    ******    ******    ******    ******    ******    ******    ******    ******    ******
-
 
 
 __author__ = "Nitzan Maarek"
@@ -40,6 +39,7 @@ class FacebookCrawler(Method_Executor):
         self._account_user_pw = self._config_parser.eval(self.__class__.__name__, "account_user_pw")
         self._group_id = self._config_parser.eval(self.__class__.__name__, "group_id")
         self._group_name = self._config_parser.eval(self.__class__.__name__, "group_name")
+        self._user_id = self._config_parser.eval(self.__class__.__name__, "user_id")
         self._domain = 'Facebook'
 
         options = webdriver.FirefoxOptions()
@@ -47,16 +47,23 @@ class FacebookCrawler(Method_Executor):
         self.driver = webdriver.Firefox(executable_path=r'C:\Python27\geckodriver.exe', firefox_options=options)
         self.driverWait = WebDriverWait(self.driver, 10)   # Waiting threshold for loading page is 10 seconds
 
+    def __del__(self):
+        print("FacebookCrawler destroyed.")
+
     def _facebook_login(self):
         """
         Method logs into facebook in facebook homepage.
         """
-        self.driver.get("https://www.facebook.com/")
-        self.driver.find_element_by_xpath("//input[@id='email']").send_keys(self._account_user_name)
-        self.driver.find_element_by_xpath("//input[@id='pass']").send_keys(self._account_user_pw)
-        self.driver.find_element_by_xpath("//input[starts-with(@id, 'u_0_')][@value='Log In']").click()
+        if self.driver.current_url == 'about:blank':
+            cookie_loaded = self._load_cookies()
+            if cookie_loaded == 'Failed':
+                self.driver.get("https://www.facebook.com/")
+                self.driver.find_element_by_xpath("//input[@id='email']").send_keys(self._account_user_name)
+                self.driver.find_element_by_xpath("//input[@id='pass']").send_keys(self._account_user_pw)
+                self.driver.find_element_by_xpath("//input[starts-with(@id, 'u_0_')][@value='Log In']").click()
+                self._save_cookies()
 
-    def extract_number_of_friends(self):
+    def get_number_of_friends_from_group_members(self):
         """
         Method extract number of friends of all member in the group in config.ini
         Updates/Saves number of friends to facebook_author table.
@@ -65,17 +72,18 @@ class FacebookCrawler(Method_Executor):
         records = self._db.get_authors_by_facebook_group(self._group_id)
         for record in records:
             user_id = record.destination_author_guid
-            num_of_friends = self._get_number_of_friends(user_id)
+            num_of_friends = self.get_number_of_friends_from_user(user_id)
             print("User id is: " + user_id + " ; Number of friends: " + num_of_friends)
             #TODO: Need to check with aviad about the author table - Need to add number of friends to DB.
 
-    def _get_number_of_friends(self, user_id):
+    def get_number_of_friends_from_user(self, user_id):
         """
         Method finds the text on a profile page "Friends <num>" on side bar of 'Intro' (not always there),
         'Photos' and 'Friends'.
         :param user_id: User profile page
         :return: The number of friends, or 'User Blocked' if we can't see
         """
+        self._facebook_login()
         self.driver.get('https://www.facebook.com/' + user_id)
         time.sleep(2)
         self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
@@ -96,14 +104,15 @@ class FacebookCrawler(Method_Executor):
         print(number_of_friends_span[0].text)
         return number_of_friends_span[0].text
 
-    def extract_members_from_group(self):
+    def get_group_members(self):
         """
         Method logs in to facebook and extracts members from given group in config.ini
+        Extract from elements under the 'div' with id = 'groupsMemberSection_recently_joined'
         Saves all members (Users, pages) of the group and the group itself as Authors.
         Saves connection from group to member as Group-Member.
         """
         self._facebook_login()
-        users_id_to_name_dict = self._get_members_from_group('https://www.facebook.com/groups/' + self._group_id + '/members/', number_of_scrolls=3)
+        users_id_to_name_dict = self._get_users_from_group_link('https://www.facebook.com/groups/' + self._group_id + '/members/', number_of_scrolls=3, id='groupsMemberSection_recently_joined')
         authors = self._convert_group_members_to_author(users_id_to_name_dict)
         authors.append(self._convert_group_to_author())
         self._db.addPosts(authors)  # Add members to Authors table in DB
@@ -142,7 +151,6 @@ class FacebookCrawler(Method_Executor):
             connections.append(connection)
         return connections
 
-
     def _convert_group_to_author(self):
         """
         Method takes given group_id from config.ini and creates Author object for it.
@@ -170,27 +178,28 @@ class FacebookCrawler(Method_Executor):
             authors.append(author)
         return authors
 
-
-    def extract_group_admins(self):
+    def get_group_admins(self):
         """
         Method logs in to facebook and extracts admin members from the group_id in config.ini
+        Extract from elements under the 'div' with id = 'groupsMemberSection_admins_moderators'
         :return: dictionary (key = user_id, value = user_name)
         """
         self._facebook_login()
-        users_id_to_name_dict = self._get_members_from_group('https://www.facebook.com/groups/'
-                                                                     + self._group_id + '/admins/')
+        users_id_to_name_dict = self._get_users_from_group_link('https://www.facebook.com/groups/'
+                                                                + self._group_id + '/admins/', id='groupsMemberSection_admins_moderators')
         return users_id_to_name_dict
 
-    def _get_group_number_of_members(self):
+    def get_group_number_of_members(self):
         """
         Method gets from span tag in class "_grt _50f8" the text = number of members
         :return: string number of members
         """
+        self._facebook_login()
         self.driver.get('https://www.facebook.com/groups/' + self._group_id + '/members/')
         num_of_members = self.driver.find_elements_by_xpath("//span[@class='_grt _50f8']")[0].text
-        return num_of_members
+        print(num_of_members)
 
-    def _get_members_from_group(self, link, number_of_scrolls=3):
+    def _get_users_from_group_link(self, link, number_of_scrolls=10, id='groupsMemberSection_recently_joined'):
         """
         Method goes to the members page of group.
         Scrolls number of 'number of scrolls' down the page
@@ -201,8 +210,8 @@ class FacebookCrawler(Method_Executor):
         for i in range(0, number_of_scrolls):
             self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
             time.sleep(3)  # Seconds between each scroll (think of the page reloading)
-
-        divs = self.driver.find_elements_by_xpath("//div[@class='uiProfileBlockContent _61ce']")
+        divs = self.driver.find_elements_by_xpath("//div[@id='" + id + "']")
+        divs = divs[0].find_elements_by_xpath(".//div[@class='uiProfileBlockContent _61ce']")
         users_id_to_name_dict = {}
 
         for div in divs:
@@ -216,37 +225,115 @@ class FacebookCrawler(Method_Executor):
 
         return users_id_to_name_dict
 
-    def extract_about_info_from_user(self):
+    def get_about_info_from_group_members(self):
         self._facebook_login()
-        records = self._db.get_authors_by_facebook_group(self._group_id)
-        about_info = None
+        group_author_guid = self._db.get_author_guid_by_facebook_osn_id(self._group_id)[0].author_guid
+        records = self._db.get_authors_by_facebook_group(group_author_guid)
+        authors = []
         for record in records:
-            user_id = record.destination_author_guid
-            about_info = self._collect_about_info_from_user(user_id)
-            #TODO: Need to save about info in DB here.
-            print(about_info)
+            user_author_guid = record.destination_author_guid
+            user_id = self._db.get_facebook_author_by_author_guid(user_author_guid)[0].author_osn_id
+            author = self.get_about_info_from_user(user_id, get_from_one_user=False)
+            authors.append(author)
+        self._db.addPosts(authors)
 
-        #TODO: Need to fix the authors table so that we can save about data.
-
-    def _collect_about_info_from_user(self, user_id):
+    def get_about_info_from_user(self, user_id=None, get_from_one_user=True):
         """
         Method browses the About page in user profile.
         Collects all details from all tabs.
-        :return: dictionary: Keys = the about tabs. Values = dictionary: keys = subjects, values = values.
+        :return: Author with all 'about' info in it
         example: {education: {COLLEGE: University of..., HIGH SCHOOL: Vernon Hills High School}, living: {...}..}
         """
-        #TODO: Need to fix "Life Events"
-        about_tabs = {'education': {}, 'living': {}, 'contact-info': {}, 'relationship': {}, 'bio': {}, 'year-overviews': {}}
+        self._facebook_login()
+        if user_id is None:
+            user_id = self._user_id
+        about_tabs = {'education': {}, 'living': {}, 'contact-info': {}, 'relationship': {}, 'bio': {}}
         for tab in about_tabs:
             self.driver.get('https://www.facebook.com/' + user_id + '/about?section=' + tab)
-            user_about_info = self.driver.find_elements_by_xpath("//div[@class='_4qm1']")
+            if self.driver.current_url.__contains__('www.facebook.com/pg/'):
+                return
+            user_about_info = self.driver.find_elements_by_xpath("//div[starts-with(@class, '_4qm1')]")
             parsed_info_dic = {}
             for info in user_about_info:
                 txt = info.text
                 txt = txt.split('\n')
-                parsed_info_dic[txt[0]] = txt[1]    # txt[0] = COLLEGE, txt[1] = University of Illinois at Urbana-Champaign
+                parsed_info_dic[txt[0]] = txt[1:]    # txt[0] = COLLEGE, txt[1] = University of Illinois at Urbana-Champaign
             about_tabs[tab] = parsed_info_dic
-        return about_tabs
+        print(about_tabs)
+        time.sleep(3) #TODO: Play with this to avoid blocking.
+        author = self._convert_about_info_to_author(user_id, about_tabs)
+        if get_from_one_user:
+            authors = [author]
+            self._db.addPosts(authors)  # Add group to member connections to AuthorConnection table in DB
+        else:
+            return author
+
+    def _convert_about_info_to_author(self, user_id, about_info_dict):
+        """
+        Method converts given about details to Author object.
+        :return: Author
+        """
+        author = Author()
+        author.domain = self._domain
+        author.author_osn_id = user_id
+        author.author_type = 'User'
+        author.education = 'User Blocked'
+        author.professional_skills = 'User Blocked'
+        author.past_residence = 'User Blocked'
+        author.birth_day = 'User Blocked'
+        author.gender = 'User Blocked'
+        author.gender = 'User Blocked'
+        author.email = 'User Blocked'
+
+        records = self._db.get_author_guid_by_facebook_osn_id(user_id)
+        if len(records) > 0:
+            author.name = records[0].name
+            author.author_guid = records[0].author_guid
+        else:
+            self.driver.get('https://www.facebook.com/' + user_id)
+            name = self.driver.find_element_by_xpath("//a[@class='_2nlw _2nlv']").text     #Extracting name
+            author.name = name
+            author.author_guid = commons.compute_author_guid_by_author_name(author.name)
+
+        if 'WORK' in about_info_dict['education']:
+            work_info = about_info_dict['education']['WORK'][0:]
+            author.work = ' '.join(work_info)
+
+        if 'EDUCATION' in about_info_dict['education']:
+            education_info = about_info_dict['education']['EDUCATION'][0:]
+            author.education = ' '.join(education_info)
+
+        if 'PROFESSIONAL SKILLS' in about_info_dict['education']:
+            author.professional_skills = about_info_dict['education']['PROFESSIONAL SKILLS'][0]
+
+        if 'CURRENT CITY AND HOMETOWN' in about_info_dict['living']:
+            current_residence_details = about_info_dict['living']['CURRENT CITY AND HOMETOWN'][0:]
+            author.current_residence = ' '.join(current_residence_details)
+
+        if 'OTHER PLACES LIVED' in about_info_dict['living']:
+            author.past_residence = about_info_dict['living']['OTHER PLACES LIVED'][0]
+
+        if 'Birth Date' in about_info_dict['contact-info']['BASIC INFORMATION'] or 'Birth Year' in about_info_dict['contact-info']['BASIC INFORMATION']:
+            birth_date_index = about_info_dict['contact-info']['BASIC INFORMATION'].index('Birth Date')
+            birth_year_index = about_info_dict['contact-info']['BASIC INFORMATION'].index('Birth Year')
+            birth_day = ""
+            if birth_date_index > -1:
+                birth_day = about_info_dict['contact-info']['BASIC INFORMATION'][birth_date_index + 1]
+            if birth_year_index > -1:
+                birth_day += about_info_dict['contact-info']['BASIC INFORMATION'][birth_date_index + 1]
+            author.birth_day = birth_day
+
+        if 'Gender' in about_info_dict['contact-info']['BASIC INFORMATION']:
+            gender_index = about_info_dict['contact-info']['BASIC INFORMATION'].index('Gender')
+            author.gender = about_info_dict['contact-info']['BASIC INFORMATION'][gender_index + 1]
+
+        if 'Email' in about_info_dict['contact-info']['CONTACT INFORMATION']:
+            email_index = about_info_dict['contact-info']['BASIC INFORMATION'].index('Email')
+            author.email = about_info_dict['contact-info']['BASIC INFORMATION'][email_index + 1]
+
+        author.relationship_status = about_info_dict['relationship']['RELATIONSHIP'][0]
+
+        return author
 
     def parse_member_id(self, txt):
         """
@@ -256,4 +343,29 @@ class FacebookCrawler(Method_Executor):
         indx_ref = txt.find("&ref")
         return txt[indx_member_id + 10: indx_ref]
 
+    def _save_cookies(self):
+        try:
+            fp_name = '{}.ProfileName'.format(self._account_user_name.replace('@', ''))
+            self.open_if_not_exists(fp_name)
+            self.driver.get('http://www.facebook.com')
+            pickle.dump(self.driver.get_cookies(), open("{}/Cookies.pkl".format(fp_name), "wb"))
+            return 'Success'
+        except:
+            return 'Failed'
 
+    def _load_cookies(self):
+        try:
+            fp_name = '{}.ProfileName'.format(self._account_user_name.replace('@', ''))
+            self.driver.get('http://www.facebook.com')
+            for cookie in pickle.load(open("{}/Cookies.pkl".format(fp_name), "rb")):
+                self.driver.add_cookie(cookie)
+            return 'Success'
+        except:
+            print('Failed to load cookies')
+            return 'Failed'
+
+    def open_if_not_exists(self, directory):
+        # if not os.path.exists(file_path):
+        #     with open(file_path, 'w'): pass
+        if not os.path.exists(directory):
+            os.makedirs(directory)
